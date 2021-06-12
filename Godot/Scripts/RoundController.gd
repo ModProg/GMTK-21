c545fe9162246cd9e3e4fe52a6d7e0ea80a04c9a
douglasasted <douglas.tavares.ast@gmail.com>
2021-06-12 00:49:27 -0300

    Fix bugs in rounds

--- Godot/Scripts/RoundController.gd
+++ Godot/Scripts/RoundController.gd
@@ -6,13 +6,13 @@ onready var healthText = get_parent().get_parent().get_node("UIContainer").get_n
 onready var enemyScene = preload("res://Scenes/Enemy.tscn")
 const maxRound = 10
 const roundTime = 10
-const spawnTime = 1
+const spawnTime = 0.75
 const health = 100
-const enemies = 30
+const enemies = 5
 
 var currentRound = 0
 var cur_enemies = 0
-var cur_roundTime = 0.1
+var cur_roundTime = 0
 var cur_spawnTime = 0
 var cur_health = 100
 
@@ -20,6 +20,8 @@ var end = false
 
 func _ready():
 	cur_roundTime = roundTime
+	roundText.text = 'Round: ' + str(currentRound) + ' / ' + str(maxRound)
+	
 	cur_spawnTime = spawnTime
 
 func _process(delta):
@@ -30,15 +32,16 @@ func _process(delta):
 	cur_spawnTime -= delta
 	if cur_roundTime < 0:
 		NewRound()
-	elif cur_spawnTime < 0 and cur_roundTime != 0:
+	elif cur_spawnTime < 0 and cur_roundTime != 0 and cur_enemies != 0:
 		SpawnEnemy()
 
 func SpawnEnemy():
 	cur_spawnTime = spawnTime
+	cur_enemies -= 1
 	
 	var enemy = enemyScene.instance()
 	
-	enemy.element = 1
+	enemy.element = int(floor(rand_range(0, 3)))
 	
 	add_child(enemy)
 
